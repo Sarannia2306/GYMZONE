5ec93cbab07df4765260c80ab6d90b6a88a9ea73
Sarannia2306 <90039978+Sarannia2306@users.noreply.github.com>
2023-09-10 19:57:08 +0800

    Add files via upload

--- homepage.php
+++ homepage.php
@@ -4,25 +4,67 @@
     <head>
         <meta charset="UTF-8">
         <title>PureGYM</title>
+        
+        <link rel = "stylesheet" type = "text/css" href = "homepage.css"/>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+        <!-- Add icon library -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    
     </head>
     <body>
+
         <?php
-        print
-       "<h1>Welcome to PureGYM!</h1>";
+      
+       
         ?>
         
-        <div class = "TopNavigation">
-            <a class = "active" href = "homepage.php">PureGYM</a>
-            <a href = "log_in.php">LOG IN</a>
-            <a href = "menu.php">MENU</a>
-            <a href = "contact.php">Contact Us</a>
-            <a href = "sign_up.php">JOIN NOW</a>
-            <a href = "booking.php">Book Your Classes NOW</a>
-            <a href = "blog.php">Blog</a>
-            <a href = "testimonial.php">Testimonial</a>
-            <a href = "pricing.php">Plans & Prices</a>
+        <header>
+            
+            <div class="logo">
+                <img src="logo.png" style="width: 120px; height: 120px; margin-left: 70px;">
+                <h1 style="font-size:300%; margin-top: -90px; margin-left: 230px;">PureGYM</h1>
+            </div>
+            
+            <div class="login">
+                <a href = "log_in.php" id="login"> 
+                    <span class="icon1"><i class="material-icons">login</i></span>LOG IN</a>
+            </div>
+     
+        </header>
+        
+        <div id="mySidenav" class="sidenav" >
+          
+            
+            <a href = "menu.php" id="menu">
+                 MENU<span class="icon"><i class="material-icons">menu</i></span></a>
+            <a href = "contact.php" id="contact">
+                 CONTACT US<span class="icon"><i class="material-icons">phone</i></span></a>
+            <a href = "sign_up.php" id="join">
+                 JOIN NOW<span class="icon"><i class="material-icons">link</i></span></a>
+            <a href = "booking.php" id="booking">
+                 CLASS BOOKING<span class="icon"><i class="material-icons">book_online</i></span></a>
+            <a href = "blog.php" id="blog">
+                 BLOG<span class="icon"><i class="material-icons">feed</i></span></a>
+            <a href = "testimonial.php" id="testimonial">
+                 TESTIMONIAL<span class="icon"><i class="material-icons">comment</i></span></a>
+            <a href = "pricing.php" id="prices">
+                 PLANS & PRICES<span class="icon"><i class="material-icons">sell</i></span></a>
             
         </div>
         
+        <div id="main" style="margin-top: 50px; margin-left: 120px;">
+            <h2> About Us</h2>
+            <p> Read more about us</p>
+        </div>
+        <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+        <footer>
+            <div class="social-icons">
+                <a href="#"><i class="fa fa-facebook"></i></a>
+                <a href="#"><i class="fa fa-instagram"></i></a>
+                <a href="#"><i class="fa fa-youtube-play"></i></a>
+            </div>
+            <p>&copy; 2023 PureGYM</p>
+            
+        </footer>
     </body>
 </html>
